8daaf427005fd7b23869621e56e27405abc8da64
nikolaskaniky <pascugeorgenicolae@yahoo.com>
2022-02-21 14:34:04 -0500

    tab logo

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        <title>GEAR SLOT BERCENI</title>
+        <link rel="shortcut icon" type="image/x-icon" href="logo.png" />
       </Head>
       <body>
         <Main />
